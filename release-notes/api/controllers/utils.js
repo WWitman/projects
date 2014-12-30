2aa17fbbbda61a19759007400bf5048fb667319e
wwitman <wwitman@apigee.com>
2014-12-30 13:14:56 -0700

    refactor

--- release-notes/api/controllers/utils.js
+++ release-notes/api/controllers/utils.js
@@ -3,28 +3,56 @@
 
 var fs = require('fs');
 var config = require('../../config/secrets').user;
-//var repo = require('../../config/repo').repository.name;
 
 module.exports = {
   formatDate: formatDate,
-  writeTitle: writeTitle
+  writeTitle: writeTitle,
+  clean: clean,
+  parseComments: parseComments,
+  getVersionFromTag: getVersionFromTag
 }
 
 
-function formatDate(datestr, err) {
-   if(!err) {
-       var date = new Date(datestr);
-       return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
-   } else {
-       console.log(err);
-       throw err;
-   }
+function formatDate(datestr) {
+    var date = new Date(datestr);
+    var month = date.getMonth() + 1;
+    return date.getFullYear() + "-" + month + "-" + date.getDate();
 }
 
-function writeTitle(owner, repo) {
+function writeTitle(owner, repo, callback) {
     var heading = "# " + owner + "/" + repo + "\n\n";
-    fs.appendFile('./relnotes/RELEASE_NOTES-'+repo+'.md', heading, function(err) {
-       if (err) throw err;
+    var file = './relnotes/RELEASE_NOTES-'+repo+'.md';
+    fs.appendFile(file, heading, function(err) {
+       if (err) {
+           callback(err)
+       } else {
+           callback(null, "Title was written to file: " + file + "\n");
+       }
     });
+
 }
 
+function clean(file, callback) {
+   fs.unlink(file, function (err) {
+      if (err) {
+           callback("Nothing to clean up: Release note file: " + file + " does not exist.\n");
+      } else {
+           callback(null, 'Successfully deleted release note file: ' + file + "\n");
+      }
+  });
+}
+
+function parseComments(data) {
+   data = data.replace(/\*/g, "");
+   data = data.trim();
+   var pattern = /\n\n/g;
+   var res = data.replace(pattern, "\n    >");
+   return res;
+}
+
+function getVersionFromTag(data, index) {
+        var version = data[index].ref;
+        var s = version.split("/");
+        version = s[s.length-1];
+        return version;
+}
