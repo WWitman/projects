35a8e053cb76c2eb5adba3ae08ac4d191f69663a
wwitman <wwitman@apigee.com>
2014-12-31 09:40:31 -0700

    added a127

--- release-notes/config/dependencies.js
+++ release-notes/config/dependencies.js
@@ -6,7 +6,7 @@
 exports.owners = [{"name":"apigee-127"}, {"name":"swagger-api"}];
 
 
-exports.a127_deps = ["swagger-converter", "swagger-tools", "volos-swagger", "apigeetool", "swagger-editor-for-apigee-127", "usergrid-installer", "volos-management-apigee", "a127-magic", "apigee-remote-proxy"];
+exports.a127_deps = ["a127", "swagger-converter", "swagger-tools", "volos-swagger", "apigeetool", "swagger-editor-for-apigee-127", "usergrid-installer", "volos-management-apigee", "a127-magic", "apigee-remote-proxy"];
 
 
 
